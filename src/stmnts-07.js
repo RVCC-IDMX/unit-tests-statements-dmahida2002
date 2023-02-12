818987ebbc87b9a2a73ab2fdbbcdd179cb393ea5
Deeshan M <runner3222@gmail.com>
2023-02-12 17:34:19 -0500

    Modified all "src" files

--- src/stmnts-07.js
+++ src/stmnts-07.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-tabs */
+/* eslint-disable no-plusplus */
 /*
  * stmnts-07.js
  * Language: javascript
@@ -13,7 +15,11 @@
  * ? [JavaScript: The Definitive Guide, Chapter 4.10](https://bit.ly/39lbxnK)
  */
 function logicalAnd(a, b) {
-  // write your code here & return
+  if (a && b) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
@@ -23,7 +29,11 @@ function logicalAnd(a, b) {
  * @returns {boolean} - true if a or b is truthy, false if both are falsy
  */
 function logicalOr(a, b) {
-  // write your code here & return
+  if (a || b) {
+    return true;
+  }
+
+  return false;
 }
 
 /**
@@ -32,7 +42,11 @@ function logicalOr(a, b) {
  * @returns {boolean} - the opposite of the given boolean
  */
 function invertBoolean(bool) {
-  // write your code here & return
+  if (bool) {
+    return false;
+  }
+
+  return true;
 }
 
 /**
@@ -49,7 +63,15 @@ function invertBoolean(bool) {
  * ? [JavaScript: The Definitive Guide, Chapter 5.4.3](https://bit.ly/39hrIlW)
  */
 function numberOfOdds(num) {
-  // write your code here & return
+  let count = 0;
+
+  for (let i = 0; i < num; i++) {
+    if (i % 2 !== 0) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 /**
@@ -62,7 +84,13 @@ function numberOfOdds(num) {
  * ? For example, num is 4 then return 10 because 1 + 2 + 3 + 4 = 10.
  */
 function addUpTheNumbers(num) {
-  // write your code here & return
+  let total = 0;
+
+  for (let i = 0; i <= num; i++) {
+    total += i;
+  }
+
+  return total;
 }
 
 /**
@@ -81,7 +109,23 @@ function addUpTheNumbers(num) {
  * ?
  */
 function gradeGenerator(score) {
-  // write your code here & return
+  if (score < 60) {
+    return 'F';
+  }
+
+  if (score < 70) {
+    return 'D';
+  }
+
+  if (score < 80) {
+    return 'C';
+  }
+
+  if (score < 90) {
+    return 'B';
+  }
+
+  return 'A';
 }
 
 /**
@@ -97,11 +141,15 @@ function gradeGenerator(score) {
  * ? return a string written like:
  * ? Francine got an A
  * ? David got a B
- * ? note: you have to use English grammar's correct indefinite article 
+ * ? note: you have to use English grammar's correct indefinite article
  * ? it's 'an A' (not a A) and 'an F' (not a F)
  */
 function getGrade(name, score) {
-  // write your code here & return
+  if (gradeGenerator(score) === 'A' || gradeGenerator(score) === 'F') {
+    return `${name} got an ${gradeGenerator(score)}`;
+  }
+
+  return `${name} got a ${gradeGenerator(score)}`;
 }
 
 module.exports = {
